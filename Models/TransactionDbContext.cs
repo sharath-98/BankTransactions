8020ef2e1899fffebca3c7fdc1cc144646913762
sharath-98 <sharathchander.p@gmail.com>
2023-05-13 12:04:45 -0500

    Added Database connection, Table Creation, DbContext and dependcy injection for TransactionDatabase

---
+++ Models/TransactionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactions.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        // Dependency injection
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {   
+        }
+
+        // Map the transaction model to the physical database
+        // Create Table TRANSACTIONS
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using BankTransactions.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Dependency injection for the context
+// pass the constructor the connection string and type of database provider 'MSSQL'
+builder.Services.AddDbContext<TransactionDbContext>( options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
 var app = builder.Build();
 
